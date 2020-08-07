f3d4fecf21f5c69f5d5164df599c5cf6c35e2a2a
Rafael Goulart <faelplg@gmail.com>
2020-08-07 01:00:34 -0300

    Updating the layout.

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,18 @@
 import Head from 'next/head'
 
-import utilStyles from '../styles/global.module.scss';
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles['container']}>
       <Head>
         <title>fael.tech | Experience for Web</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <div>Hero</div>
-        <div>Content</div>
+      <main className={styles['main']}>
+        <div className={styles['main__hero']}>Hero</div>
+        <div className={styles['main__content']}>Content</div>
       </main>
-        <style jsx>{`
-          .container {
-            padding: 1.6rem;
-          }
-        `}</style>
     </div>
   )
 }
