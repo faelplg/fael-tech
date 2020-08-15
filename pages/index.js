82b75391d64b1be5ea481fda9015513b51d3d2ef
Rafael Goulart <faelplg@gmail.com>
2020-08-14 23:56:50 -0300

    Updated styles structure and added a logo.

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import styles from './home.module.scss';
 
 export default function Home() {
+  const flex = 'flex';
   return (
     <div className={styles['container']}>
       <Head>
@@ -10,8 +11,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles['main']}>
-        <div className={styles['main__hero']}>Hero</div>
-        <div className={styles['main__content']}>Content</div>
+        <div className={[styles['main__hero'], 'enterFromLeft'].join(' ')}>
+          <div className={styles['hero__logo']}>
+            <img src="/img/logo/main.png" alt="Fael.Tech logo."/>
+          </div>
+          <div className={styles['hero__footer']}>
+          </div>
+        </div>
+        <div className={styles['main__content']}></div>
       </main>
     </div>
   )
